3b7fb651a5a571207bfa57f9c7057a2c7b0e8e1c
zeynepdundarr <idundar17@ku.edu.tr>
2024-04-03 20:00:31 +0300

    add optimization to the project

--- src/crawler/pdf_crawler.py
+++ src/crawler/pdf_crawler.py
@@ -1,31 +1,97 @@
 from src.crawler.pdf_processing import filter_pdf_links
-from ..extraction.pdf_link_extraction import get_all_links_with_timeout
+from ..extraction.pdf_link_extraction import get_all_links_with_timeout, get_all_links
+import aiohttp
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 
-def crawl_for_pdfs(company_name, website, url, depth, visited=None, attempts=3):
+async def get_all_links_with_timeout(session, url):
+    # Set a timeout for the request
+    timeout = aiohttp.ClientTimeout(total=10)
+
+    try:
+        if url == "https://www.pekaobiznes24.pl/do/LangSelect":
+            print("Skipping problematic URL")
+            return []
+        else:
+            links = []
+        async with session.get(url, timeout=timeout) as response:
+            # Only parse the page if the content type is HTML
+            if 'text/html' in response.headers.get('Content-Type', ''):
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                for link in soup.find_all('a', href=True):
+                    full_link = urljoin(url, link['href'])
+                    links.append(full_link)
+                return links
+                # content = await response.text()
+                # links = await parse_links(content)
+                # return links  # Return a list of links
+    except Exception as e:
+        print(f"Request to {url} failed: {e}")
+        return []
+
+async def parse_links(html_content):
+    # Given the HTML content, return all the href links found
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = [a.get('href') for a in soup.find_all('a', href=True)]
+    return links
+
+async def crawl_for_pdfs(company_name, website, url, depth, session, visited=None, attempts=3):
     if visited is None:
         visited = set()
     if url in visited or depth == 0:
         return []
     
-    print(f"Crawling: {url}")
     visited.add(url)
     pdf_links = []
-    
+
     if len(visited) > 50: 
         return list(set(pdf_links))
     
     for _ in range(attempts):
-        links = get_all_links_with_timeout(url)
+        links = await get_all_links_with_timeout(session, url)
         if links:
             break
     else:
         print("Timeout - skipping this url: ", url)
         return list(set(pdf_links))
-    
+
     pdf_links.extend(filter_pdf_links(company_name, website, links))
-    
+
     for link in links:
         if link not in visited:
-            pdf_links.extend(crawl_for_pdfs(company_name, website, link, depth - 1, visited))
-    
+            pdf_links_from_recursive_call = await crawl_for_pdfs(company_name, website, link, depth - 1, session, visited)
+            pdf_links.extend(pdf_links_from_recursive_call)
+
     return list(set(pdf_links))
+
+    
+# def crawl_for_pdfs(company_name, website, url, depth, visited=None, attempts=3):
+#     if visited is None:
+#         visited = set()
+#     if url in visited or depth == 0:
+#         return []
+    
+#     print(f"Crawling: {url}")
+#     visited.add(url)
+#     pdf_links = []
+    
+#     if len(visited) > 50: 
+#         return list(set(pdf_links))
+    
+#     for _ in range(attempts):
+#         links = get_all_links_with_timeout(url)
+#         if links:
+#             break
+#     else:
+#         print("Timeout - skipping this url: ", url)
+#         return list(set(pdf_links))
+    
+#     pdf_links.extend(filter_pdf_links(company_name, website, links))
+    
+#     for link in links:
+#         if link not in visited:
+#             pdf_links.extend(crawl_for_pdfs(company_name, website, link, depth - 1, visited))
+    
+#     return list(set(pdf_links))
--- src/main.py
+++ src/main.py
@@ -1,57 +1,31 @@
 import time
+
+import aiohttp
 from src.extraction.company_name_extractor import extract_company_names_from_excel
 from src.crawler.pdf_crawler import crawl_for_pdfs
 from src.extraction.company_website_extractor import find_company_website
+import asyncio
 
-def main():
+async def main():
     company_names = extract_company_names_from_excel("./data/input/ai_automation_project.xlsx")
-    process_company_pdfs(company_names)
+    await process_company_pdfs(company_names)
 
-def process_company_pdfs(company_names):
+async def process_company_pdfs(company_names):
 
     company_websites = [('Segro', 'https://www.segro.com/')]
-           
-    for (name, website) in company_websites:
-        if website != "Website not found.":
-            crawl_for_pdfs(name, website, website, depth=2)
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for (name, website) in company_websites:
+            if website != "Website not found.":
+                tasks.append(crawl_for_pdfs(name, website, website, depth=2, session=session))
 
-    # # SERP API key Free version is invalid
-    # # uncomment this when SERP API KEY is present
-    # company_website_pdfs = {}
-    # for name in company_names:
-    #     website = find_company_website(name)
-    #     company_website_pdfs[(name, website)] = []
-    #     if website != "Website not found.":
-    #         crawl_for_pdfs(name, website, website, depth=2)
-            
-    
-    # # mock data is presented in the same format that I obtained from the code patch above, according to the SERP API"
-    # company_websites = [('Bank Pekao', 'https://www.pekao.com.pl/'),
-    #  ('Latour', 'Not found'),
-    #  ('Renault', 'https://www.group.renault.com/'),
-    #  ('Segro', 'https://www.segro.com/'),        
-    # ('Freenet', 'https://www.freenet-group.de/'),
-    # ('Acciona', 'https://www.acciona.com/'),
-    # ('Caixabank', 'https://www.caixabank.com/'),
-    # ('Hexpol', 'https://www.hexpol.com/'),
-    # ('Bankinter', 'https://www.bankinter.com/'),
-    # ('Temenos', 'https://www.temenos.com/'),
-    # ('BAE Systems', 'https://www.baesystems.com/'),
-    # ('ACS', 'https://www.grupoacs.com/'),
-    # ('', 'https://www.pandoragroup.com/'),
-    # ('Aveva', 'https://www.aveva.com/'),
-    # ('HomeServe', 'Not found'),
-    # ('Accor', 'https://group.accor.com/'),
-    # ('Wendel', 'https://www.wendelgroup.com/en'),
-    # ('Reply', 'https://www.reply.com/'),
-    # ('Barratt Developments', 'https://www.barrattdevelopments.co.uk/'),
-    # ('Inditex', 'https://www.inditex.com/')]
-   
+        await asyncio.gather(*tasks)
      
 
 if __name__ == "__main__":
     start_time = time.time()
-    main()
+    # main()
+    asyncio.run(main())
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time} seconds")
